1567fa3819ad748ad55b586464894e254cbc386e
Yigit Arslan <yigitarsland@icloud.com>
2025-12-04 13:50:20 +0100

    chore: environment variable configuration

--- disambiguate_authors.py
+++ disambiguate_authors.py
@@ -2,11 +2,22 @@ import pandas as pd
 import networkx as nx
 from sqlalchemy import create_engine, text
 import textdistance
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Fetch DB credentials from environment
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 # ==============================================================================
 # CONFIGURATION
 # ==============================================================================
-DB_CONNECTION = "postgresql+psycopg2://username:password@localhost:5432/your_db"
+DB_CONNECTION = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_CONNECTION)
 
 # THRESHOLDS
